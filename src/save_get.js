b6b311dff8721117d0ae75378b8828d94cddc6f9
sambeck87 <sandro_mipb@hotmail.com>
2022-12-19 19:47:47 -0600

    Add index.js file Add index.html file Add style.css fie Add save_get file Add add file Add add function Add get and save function

---
+++ src/save_get.js
@@ -0,0 +1,32 @@
+function save(data) {
+  data = JSON.stringify(data)
+  localStorage.setItem('scores', data);
+}
+
+function get() {
+  let allScores = localStorage.getItem('scores');
+  allScores = JSON.parse(allScores);
+  if(!allScores) {
+    return null;
+  }
+  return allScores;
+}
+
+function Display() {
+  const allScores = get();
+  let toDisplay = ``;
+  if (!allScores) {
+    return;
+  }
+  for (let index = 0; index < allScores.length; index++) {
+    let background = '';
+    if (index % 2) {
+      background = 'grey'
+    }
+    toDisplay += `<div class="${background} score_list">${allScores[index].name}: ${allScores[index].score}</div>`
+  }
+  const contScores = document.getElementById('scores');
+  contScores.innerHTML = `${toDisplay}`;
+}
+
+export {save, get, Display}
